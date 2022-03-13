f98aecb7ce988466fbd914074aa8fdb5fe894bf0
DhanushP <38401094+Dhanush05@users.noreply.github.com>
2022-03-12 18:31:28 -0600

    Update 138_copy_list_with_random_pointer.java

--- 138_copy_list_with_random_pointer.java
+++ 138_copy_list_with_random_pointer.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    HashMap<Node,Node> copy = new HashMap<Node, Node>();
+    public Node copyRandomList(Node head) {
+        if(head == null)
+            return null;
+        if(this.copy.containsKey(head)){
+            return this.copy.get(head);
+        }
+        Node node = new Node(head.val, null, null);
+        this.copy.put(head, node);
+        node.next = this.copyRandomList(head.next);
+        node.random = this.copyRandomList(head.random);
+        return node;
+    }
+}
